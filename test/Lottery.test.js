08e9668a46d106f40e67a90ffef1895e7c1db23a
saurabh <srbh24@gmail.com>
2022-01-14 13:09:55 +0530

    added test case-money to winner

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -3,7 +3,10 @@ const ganache = require("ganache-cli");
 const Web3 = require("web3");
 const web3 = new Web3(ganache.provider());
 
-const { interface, bytecode } = require("../compile");
+const {
+  interface,
+  bytecode
+} = require("../compile");
 
 let lottery;
 let accounts;
@@ -12,8 +15,13 @@ beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
 
   lottery = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode })
-    .send({ from: accounts[0], gas: "1000000" });
+    .deploy({
+      data: bytecode
+    })
+    .send({
+      from: accounts[0],
+      gas: "1000000"
+    });
 });
 describe("Lottery Contract", () => {
   it("deploys a contract", () => {
@@ -79,5 +87,22 @@ describe("Lottery Contract", () => {
     } catch (err) {
       assert(err);
     }
+  });
+
+  it('sends money to the winner and resets the players array', async() =>{
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei('2','ether')
+    });
+
+    const initialBalance = await web3.eth.getBalance(accounts[0]);
+
+    await lottery.methods.pickWinner().send({from:accounts[0]});
+
+    const finalBalance = await web3.eth.getBalance(accounts[0]);
+
+    const difference  = finalBalance - initialBalance;
+    console.log(finalBalance - initialBalance);
+    assert(difference > web.utils.toWei('1.8','ether'));
   });
 });
